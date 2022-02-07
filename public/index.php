11498aa51f4bbffa8dfb1094da637f2ecdc1cc27
L33Z22L11 <L33Z22L11@gmail.com>
2022-02-07 18:28:07 +0800

    更新依赖

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 $type = $_REQUEST['type'];
 if (!$type) $type = 'monoroll';
-$imgs = explode("\n", file_get_contents("/$type/imgs.csv"));
+$imgs = explode("\n", file_get_contents("./$type/imgs.csv"));
 $num = $_REQUEST['num'];
 if (!$num) $num = rand(0, count($imgs) - 1);
 $img = explode(',', $imgs[$num]);
-$url = explode('{key}', file_get_contents("/$type/url.cfg"));
+$url = explode('{key}', file_get_contents("./$type/url.cfg"));
 echo <<<EOF
 {   "id": "$img[0]",
     "author": "$img[1]",
     "title": "$img[2]",
     "url": "$url[0]$img[3]$url[1]",
-    "comment": "$img[4]",
+    "comment": "$img[4]"
 }
 EOF;
