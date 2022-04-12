50f1197073773c97cbfe808f08479cf3fd2a5298
Zhilu <L33Z22L11@gmail.com>
2022-04-12 15:57:09 +0800

    Update imgurl.php

--- public/imgurl.php
+++ public/imgurl.php
@@ -6,4 +6,4 @@ $num = $_REQUEST['num'];
 if (!$num) $num = rand(0, count($imgs) - 1);
 $img = explode(',', $imgs[$num]);
 $url = explode('{key}', file_get_contents("./$type/url.cfg"));
-echo "<script>location.href='$url[0]$img[3]$url[1]'</script>";
+die(header("Location: $url[0]$img[3]$url[1]"));
